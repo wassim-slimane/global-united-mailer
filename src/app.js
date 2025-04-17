c5e694538eccc1e71d44fca4d5c8542d4351e21a
wassim-slimane <wassim.slimane01@gmail.com>
2025-04-17 13:17:05 +0200

    add slug to api endpoints

--- src/app.js
+++ src/app.js
@@ -12,18 +12,18 @@ app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Mailer App Running!'));
+app.get('/mailer', (req, res) => res.send('Mailer App Running!'));
 
-app.use('/api/check-connection', (req, res, next) => {
+app.use('/mailer/api/check-connection', (req, res, next) => {
     checkConnection().then(() => res.status(200).send('Connection Checked')).catch(next);
 });
 
 // Routes
-app.post('/api/contact', (req, res, next) => {
+app.post('/mailer/api/contact', (req, res, next) => {
     const form = req.body;
-    const sanitizedForm = sanitizeForm(form);
+    //const sanitizedForm = sanitizeForm(form);
 
-    sendEmail(sanitizedForm).then((result) => {
+    sendEmail(form).then((result) => {
         res.status(200).send(result);
     }).catch((err) => {
         res.status(400).send(err.message);
